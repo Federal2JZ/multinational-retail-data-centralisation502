73155540897f25b40e4483b79c76a677fb506087
Federal2JZ <darpanvinodrai@gmail.com>
2024-01-01 22:05:31 +0000

    Created method called read_db_creds, which will read the creds of the yaml file and returns a dictionary of credentials. Created another method called init_db_engine which reads the creds from return of previous method and returns an sqlalchemy database engine. Created list_db_tables method that lists all tables in the database. Created another method called upload_to_db which takes in a Pandas DataFrame and table name to upload as an argument.

--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,7 @@
 import psycopg2
+import yaml
+from sqlalchemy import create_engine
+
 
 class DatabaseConnector:
     def __init__(self, dbname, user, password, host):
@@ -27,6 +30,27 @@ class DatabaseConnector:
     def execute_query(self, query):
         # Method to execute a query on the database
         pass
+
+    def read_db_creds(self, file_path="db_creds.yaml"):
+        # Method to read database credentials from a YAML file
+        with open(file_path, "r") as file:
+            return yaml.safe_load(file)
+        
+    def init_db_engine(self):
+        # Method to initialize and return a SQLAlchemy database engine
+        creds = self.read_db_creds()
+        return create_engine(
+            f"postgresql://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}"
+        )
+    
+    def list_db_tables(self):
+        pass
+
+    def upload_to_db(self, dataframe, table_name):
+        # Method to upload a Pandas DataFrame to a database table
+        dataframe.to_sql(table_name, con=self.init_db_engine(), if_exists='replace', index=False)
+
+
 
 if __name__ == "__main__":
     pass
