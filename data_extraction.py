805891ff2a424d9193ef3e6b466e5d9a7f6dc90b
Federal2JZ <darpanvinodrai@gmail.com>
2024-01-04 21:45:10 +0000

    removed placeholder extractors anad improved read_rds_table method with a dataframe

--- data_extraction.py
+++ data_extraction.py
@@ -1,28 +1,12 @@
 import pandas as pd
-from database_utils import DatabaseConnector
 
 
 class DataExtractor:
     def __init__(self):
         pass
     
-    def extract_csv_data(self, file_path):
-        # Method to extract data from a CSV file
-        pass
-    
-    def extract_api_data(self, api_url, api_key):
-        # Method to extract data from an API
-        pass
-    
-    def extract_s3_data(self, bucket_name, object_key):
-        # Method to extract data from an S3 bucket
-        pass
-
     def read_rds_table(self, db_connector, table_name):
-        # Method to extract a database table to a pandas DataFrame
         engine = db_connector.init_db_engine()
-        query = f"SELECT * FROM {table_name}"
-        return pd.read_sql(query, con=engine)
-
-if __name__ == "__main__":
-    pass
+        query = f"SELECT * FROM {table_name};"
+        df = pd.read_sql_query(query, engine)
+        return df
