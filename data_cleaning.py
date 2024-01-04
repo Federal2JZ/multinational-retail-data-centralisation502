a421504c5c60451b5213a437fd3ff6e4b1cc89c7
Federal2JZ <darpanvinodrai@gmail.com>
2024-01-04 21:42:10 +0000

    populated clean_user_data method with invalid date, null m isses and drop first columns if empty

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,25 +1,7 @@
-import pandas as pd
-
-
 class DataCleaning:
-    def __init__(self):
-        pass
-    
-    def clean_csv_data(self, raw_data):
-        # Method to clean data extracted from a CSV file
-        pass
-    
-    def clean_api_data(self, raw_data):
-        # Method to clean data extracted from an API
-        pass
-    
-    def clean_s3_data(self, raw_data):
-        # Method to clean data extracted from an S3 bucket
-        pass
-
-    def clean_user_data(self, user_data):
-        # Method to clean user data
-        pass
-
-if __name__ == "__main__":
-    pass
+    def clean_user_data(self,df):
+        df = self.clean_invalid_date(df,'date_of_birth')
+        df = self.clean_invalid_date(df,'join_date')        
+        df = self.clean_NaNs_Nulls_misses(df)
+        df.drop(columns='1',inplace=True)
+        return df
