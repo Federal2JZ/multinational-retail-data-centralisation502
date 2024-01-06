78b9d98572b968924d1e345feb69cb521ee53385
Federal2JZ <darpanvinodrai@gmail.com>
2024-01-06 20:45:59 +0000

    Restructure DatabaseConnector class for better code organization
    The DatabaseConnector class has been refactored to improve code readability and organization. Changes include:
    - Imported SafeLoader from yaml.loader
    - Modified the constructor to accept a file_name parameter
    - Updated read_db_creds method to use SafeLoader
    - Updated init_db_engine method to use the read_db_creds method directly
    - Removed unnecessary variables in the upload_to_db method
    - Introduced constants for database connection parameters in the upload_to_db method

--- database_utils.py
+++ database_utils.py
@@ -1,30 +1,33 @@
 import yaml
+from yaml.loader import SafeLoader
 from sqlalchemy import create_engine, inspect
 
 
-class DatabaseConnector:
-    def __init__(self, db_name):
-        self.connection = None
-        self.cursor = None
-        self.db_name = db_name
+class DatabaseConnector():
+    def __init__(self,file_name = None):
+        self.file_name = file_name
 
-    def read_db_creds(self, creds_file='db_creds.yaml'):
-        with open(creds_file, 'r') as file:
-            creds = yaml.safe_load(file)
-        return creds
+    def read_db_creds(self):
+        with open(self.file_name) as f:
+            data = yaml.load(f, Loader=SafeLoader)
+            return data
 
     def init_db_engine(self):
-        creds = self.read_db_creds()
-        engine_string = f"postgresql+psycopg2://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}"
-        engine = create_engine(engine_string)
+        data_2 = self.read_db_creds()
+        engine = create_engine(f"postgresql+psycopg2://{data_2['RDS_USER']}:{data_2['RDS_PASSWORD']}@{data_2['RDS_HOST']}:{data_2['RDS_PORT']}/{data_2['RDS_DATABASE']}")
         return engine
-    
+
     def list_db_tables(self):
-        engine = self.init_db_engine()
-        inspector = inspect(engine)
-        tables = inspector.get_table_names()
-        return tables
-    
-    def upload_to_db(self, df, table_name):
-        engine = self.init_db_engine()
-        df.to_sql(table_name, engine, if_exists='replace', index=False)
+        inspector = inspect(self.init_db_engine())
+        return inspector.get_table_names()
+
+    def upload_to_db(self,dataframe,table_name):
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        HOST = 'localhost'
+        USER = 'postgres'
+        PASSWORD = 'admin123'
+        DATABASE = 'sales_data'
+        PORT = 5432
+        engine_2 = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+        dataframe.to_sql(table_name,engine_2,if_exists = 'replace')
