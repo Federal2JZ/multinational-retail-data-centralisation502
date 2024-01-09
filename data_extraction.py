3df3fbc8c38001316165c194e908150c9035395b
Federal2JZ <darpanvinodrai@gmail.com>
2024-01-09 21:36:46 +0000

    Added flexibility to accept a database_connector_instance and table_name during initialization for improved configurability.

--- data_extraction.py
+++ data_extraction.py
@@ -1,12 +1,15 @@
 import pandas as pd
+import tabula as tb
+import requests
 
 
-class DataExtractor:
-    def __init__(self):
-        pass
-    
+class DataExtractor():
+    def __init__(self, database_connector_instance = None, table_name = None):
+        self.database_connector_instance = database_connector_instance
+        self.table_name = table_name
+
     def read_rds_table(self, db_connector, table_name):
         engine = db_connector.init_db_engine()
-        query = f"SELECT * FROM {table_name};"
-        df = pd.read_sql_query(query, engine)
+        query = f"SELECT * FROM {table_name}"
+        df = pd.read_sql(query, engine)
         return df
