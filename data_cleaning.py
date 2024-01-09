d9d60422b8a789a8150a4b8a9a427a662aeb23ae
Federal2JZ <darpanvinodrai@gmail.com>
2024-01-09 21:32:08 +0000

    Refactor data cleaning method in DataCleaning class - Replaces 'NULL' values with NaN using NumPy for consistency. - Converts 'date_of_birth' and 'join_date' columns to datetime format using pandas with improved error handling. - Drops rows with missing values in key columns ('first_name', 'last_name', 'user_uuid') for data integrity. - Removes rows where 'date_of_birth' is in the future, ensuring data accuracy.

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,25 @@
+import pandas as pd
+from datetime import datetime
+import numpy as np
+
+
 class DataCleaning:
-    def clean_user_data(self,df):
-        df = self.clean_invalid_date(df,'date_of_birth')
-        df = self.clean_invalid_date(df,'join_date')        
-        df = self.clean_NaNs_Nulls_misses(df)
-        df.drop(columns='1',inplace=True)
-        return df
+    def __init__(self):
+        pass
+
+    def clean_user_data(self, user_data):
+        # Replace 'NULL' values with NaN
+        user_data.replace('NULL', np.nan, inplace=True)
+
+        # Convert date_of_birth and join_date columns to datetime format
+        date_columns = ['date_of_birth', 'join_date']
+        user_data[date_columns] = user_data[date_columns].apply(pd.to_datetime, errors='coerce')
+
+        # Drop rows with incorrect or missing values in key columns
+        key_columns = ['first_name', 'last_name', 'user_uuid']
+        user_data.dropna(subset=key_columns, inplace=True)
+
+        # Remove rows where date_of_birth is in the future
+        user_data = user_data[user_data['date_of_birth'] <= pd.to_datetime('today')]
+
+        return user_data
