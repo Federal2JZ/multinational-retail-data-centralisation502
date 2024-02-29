3afaf9a9441f9d1d73c8030b7c74d24dc60a8e52
Federal2JZ <darpanvinodrai@gmail.com>
2024-02-29 13:17:06 +0000

    Deleted development stage comments, and added Docstrings

--- data_extraction.py
+++ data_extraction.py
@@ -6,27 +6,78 @@ import os
 
 
 class DataExtractor():
+    """
+    A class for extracting data from various sources.
+    """
+
     def __init__(self, database_connector_instance=None, table_name=None):
+        """
+        Initialize the DataExtractor.
+
+        Parameters:
+        - database_connector_instance: An instance of the database connector.
+        - table_name: The name of the table from which data will be extracted.
+        """
         self.database_connector_instance = database_connector_instance
         self.table_name = table_name
 
     def read_rds_table(self, db_connector, table_name):
+        """
+        Read data from a table in a relational database.
+
+        Parameters:
+        - db_connector: An instance of the database connector.
+        - table_name: The name of the table to read from.
+
+        Returns:
+        - df: A pandas DataFrame containing the data from the specified table.
+        """
         engine = db_connector.init_db_engine()
         query = f"SELECT * FROM {table_name}"
         df = pd.read_sql(query, engine)
         return df
     
     def retrieve_pdf_data(self, link):
+        """
+        Retrieve data from a PDF file.
+
+        Parameters:
+        - link: The URL or path to the PDF file.
+
+        Returns:
+        - df_pdf: A pandas DataFrame containing the extracted data from the PDF.
+        """
         pdf_data = tb.read_pdf(link,pages = 'all')
         df_pdf = pd.concat(pdf_data)
         return df_pdf
     
     def list_number_of_stores(self, endpoint, headers):
+        """
+        Get the number of stores from an API endpoint.
+
+        Parameters:
+        - endpoint: The URL of the API endpoint.
+        - headers: A dictionary of headers to be included in the request.
+
+        Returns:
+        - result: The number of stores retrieved from the API.
+        """
         response = requests.get(endpoint, headers=headers)
         result = response.json().get('number_stores')
         return result
     
     def retrieve_stores_data(self, retrieve_store_endpoint, headers, num_stores):
+        """
+        Retrieve data for multiple stores from an API endpoint.
+
+        Parameters:
+        - retrieve_store_endpoint: The base URL of the API endpoint for retrieving store data.
+        - headers: A dictionary of headers to be included in the request.
+        - num_stores: The number of stores to retrieve data for.
+
+        Returns:
+        - stores_df: A pandas DataFrame containing the data for all the stores.
+        """
         stores_data = []
 
         for store_number in range(1, num_stores + 1):
@@ -48,6 +99,15 @@ class DataExtractor():
         return stores_df
     
     def extract_from_s3(self, s3_address):
+        """
+        Extract data from a CSV file stored in an S3 bucket.
+
+        Parameters:
+        - s3_address: The address of the CSV file in S3 (e.g., 's3://bucket_name/path/to/file.csv').
+
+        Returns:
+        - products_data: A pandas DataFrame containing the extracted data from the CSV file.
+        """
         s3_client = boto3.client('s3')
         products_data = None
 
@@ -60,13 +120,6 @@ class DataExtractor():
         
         # Read the CSV into a DataFrame
         products_data = pd.read_csv(local_file_name)
-
-            # # Print the shape of the DataFrame
-            # print("Products Data Shape:", products_data.shape)
-
-            # # Print information about the DataFrame after cleaning
-            # print("Products Data Information:")
-            # print(products_data.info())
 
         # Clean up: Close the local file
         products_data.to_csv(local_file_name, index=False)  # Save the cleaned DataFrame
